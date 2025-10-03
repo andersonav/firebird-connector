cf1f129a4be1c3601ab05d6d400469545490db44
Anderson Sousa <andersomsousa1406@gmail.com>
2025-10-03 13:00:13 -0300

    Add Firebird-specific methods and conventions to Schema and Query builders
    - Implemented `pluck` method in Query\Builder for retrieving column values.
    - Enhanced FirebirdGrammar with new wrapping methods for tables and values.
    - Introduced custom Blueprint class with Firebird-specific column definitions.
    - Updated FirebirdGrammar to handle auto-incrementing integer types.

--- src/Schema/Builder.php
+++ src/Schema/Builder.php
@@ -3,8 +3,19 @@
 namespace AndersonAv\Firebird\Schema;
 
 use Illuminate\Database\Schema\Builder as SchemaBuilder;
+use AndersonAv\Firebird\Schema\Blueprint;
 
 class Builder extends SchemaBuilder
 {
-    //
+    /**
+     * Create a new Blueprint instance.
+     *
+     * @param  string  $table
+     * @param  \Closure|null  $callback
+     * @return \AndersonAv\Firebird\Schema\Blueprint
+     */
+    protected function createBlueprint($table, \Closure $callback = null)
+    {
+        return new Blueprint($table, $callback);
+    }
 }
